ef7fce43aade55edc50b4e3dd75d512037e441f7
ash-uncover <ash.uncover@gmail.com>
2023-03-02 07:04:52 +0100

    playable on mobile

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,15 @@
 import React, { useEffect, ReactElement } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 // Store
 import AppSelectors from 'store/app/app.selectors'
 import AppSlice from 'store/app/app.slice'
-// Libs
-import { loadData } from 'lib/data'
 // Services
 import { useTranslation } from 'react-i18next'
 import { useWardService } from '@uncover/ward-react'
+import { AppLoadStatuses } from 'store/app/app.state'
+import AppLoading from './AppLoading'
+import { Display } from './common/display/Display'
 
 interface AppProperties {
   children: ReactElement
@@ -22,14 +23,20 @@ const App = ({
 
   const dispatch = useDispatch()
 
+  const navigate = useNavigate()
+
   const { t, i18n } = useTranslation()
 
   const query = useQuery()
-  const loaded = useSelector(AppSelectors.loaded)
+  const loadStatus = useSelector(AppSelectors.loadStatus)
 
   const language = useSelector(AppSelectors.language)
 
   useWardService(dispatch)
+
+  useEffect(() => {
+    navigate('/')
+  }, [])
 
   useEffect(() => {
     i18n.changeLanguage(language)
@@ -39,28 +46,32 @@ const App = ({
     dispatch(AppSlice.actions.setEmbedded({ embedded: query.has('embedded') }))
   }, [])
 
-  useEffect(() => {
-    loadData()
-      .then(() => dispatch(AppSlice.actions.setLoaded(true)))
-  }, [])
 
   // Rendering //
 
-  if (loaded) {
-    return children
+  switch (loadStatus) {
+    case AppLoadStatuses.NONE:
+    case AppLoadStatuses.LOADING:
+    case AppLoadStatuses.READY: {
+      return (
+        <Display className='app'>
+          <AppLoading />
+        </Display>
+      )
+    }
+    case AppLoadStatuses.STARTED: {
+      return (
+        <Display className='app'>
+          {children}
+        </Display>
+      )
+    }
   }
-
-  return (
-    <div>
-      loading
-    </div>
-  )
 }
 
 const useQuery = () => {
   const { search } = useLocation()
   return React.useMemo(() => new URLSearchParams(search), [search])
 }
-
 
 export default App
